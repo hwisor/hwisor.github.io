ef51a4c73c8877b8bd867e342a9e757a7c4a387d
hwisor <113186032+hwisor@users.noreply.github.com>
2022-10-20 21:58:42 -0400

    Exercise 6

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	//------------------------------------
+	// show hide each itme
+	//------------------------------------
+
+	$('.item').click(function(){
+		$(this).toggleClass('active');
+	});
+
+	//---------------------------
+	// setup show hide all button
+	//---------------------------
+
+	$("#showhide").click(function(){
+		
+		$('.item').toggleClass('active');
+	});
+
+});
