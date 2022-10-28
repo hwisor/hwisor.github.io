328db0d6551f9daf3291b60b3a64884d90e478af
hwisor <113186032+hwisor@users.noreply.github.com>
2022-10-28 17:05:06 -0400

    Edits!

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 $(document).ready(function(){
 
-	//------------------------------------
-	// show hide each itme
-	//------------------------------------
-
 	$('.item').click(function(){
 		$(this).toggleClass('active');
 	});
-
-	//---------------------------
-	// setup show hide all button
-	//---------------------------
 
 	$("#showhide").click(function(){
 		
